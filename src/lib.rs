93d0dab945ce719c5b4331cba0857c320cdd8387
Cameron Taggart <cameron.taggart@gmail.com>
2020-09-19 11:53:56 -0600

    add get_refs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 mod reference;
 
-use autorust_openapi::{OpenAPI, Operation, PathItem, ReferenceOr};
+use autorust_openapi::{OpenAPI, Operation, PathItem, ReferenceOr, Schema};
 use indexmap::IndexSet;
 use reference::Reference;
 use std::{fs::File, io::prelude::*};
@@ -11,14 +11,7 @@ pub type Result<T> = std::result::Result<T, Error>;
 pub fn read_api_file(path: &str) -> Result<OpenAPI> {
     let mut bytes = Vec::new();
     File::open(path)?.read_to_end(&mut bytes)?;
-    let deserializer = &mut serde_json::Deserializer::from_slice(&bytes);
-    let mut ignored: Vec<String> = Vec::new();
-    let api: OpenAPI = serde_ignored::deserialize(deserializer, |path| {
-        ignored.push(path.to_string());
-    })?;
-    if ignored.len() > 0 {
-        Err(format!("api file {} has some ignored {:?}", path, ignored))?
-    }
+    let api = serde_json::from_slice(&bytes)?;
     Ok(api)
 }
 
@@ -36,48 +29,147 @@ pub fn pathitem_operations(item: &PathItem) -> impl Iterator<Item = &Operation> 
     .filter_map(|x| x)
 }
 
-/// returns a set of rererenced files
-pub fn get_ref_files(api: &OpenAPI) -> Result<IndexSet<String>> {
-    let mut set = IndexSet::new();
+#[derive(Clone, Debug, PartialEq)]
+pub enum RefString {
+    PathItem(String),
+    Parameter(String),
+    Schema(String),
+    Example(String),
+}
 
-    let mut insert = |reference: &str| -> Result<()> {
-        let rf = Reference::parse(reference)?;
-        match rf.file {
-            Some(file) => {
-                set.insert(file);
-                ()
-            }
-            None => {}
+impl ToString for RefString {
+    fn to_string(&self) -> String {
+        match self {
+            RefString::PathItem(s) => s.to_owned(),
+            RefString::Parameter(s) => s.to_owned(),
+            RefString::Schema(s) => s.to_owned(),
+            RefString::Example(s) => s.to_owned(),
         }
-        Ok(())
-    };
+    }
+}
+
+fn add_refs_for_schema(list: &mut Vec<RefString>, schema: &Schema) {
+    for (_, schema) in &schema.properties {
+        match schema {
+            ReferenceOr::Reference { reference } => {
+                list.push(RefString::Schema(reference.to_owned()))
+            }
+            ReferenceOr::Item(schema) => add_refs_for_schema(list, schema),
+        }
+    }
+    match &schema.additional_properties {
+        Some(ReferenceOr::Reference { reference }) => {
+            list.push(RefString::Schema(reference.to_owned()))
+        }
+        Some(ReferenceOr::Item(schema)) => add_refs_for_schema(list, schema),
+        None => {}
+    }
+    match &schema.items {
+        Some(ReferenceOr::Reference { reference }) => {
+            list.push(RefString::Schema(reference.to_owned()))
+        }
+        Some(ReferenceOr::Item(schema)) => add_refs_for_schema(list, schema),
+        None => {}
+    }
+    for schema in &schema.all_of {
+        match schema {
+            ReferenceOr::Reference { reference } => {
+                list.push(RefString::Schema(reference.to_owned()))
+            }
+            ReferenceOr::Item(schema) => add_refs_for_schema(list, schema),
+        }
+    }
+}
+
+/// returns a list of refs
+pub fn get_refs(api: &OpenAPI) -> Vec<RefString> {
+    let mut list = Vec::new();
 
     // paths and operations
     for (_path, item) in &api.paths {
         match item {
-            ReferenceOr::Reference { reference } => insert(&reference)?,
+            ReferenceOr::Reference { reference } => {
+                list.push(RefString::PathItem(reference.to_owned()))
+            }
             ReferenceOr::Item(item) => {
                 for op in pathitem_operations(&item) {
+                    // parameters
                     for prm in &op.parameters {
                         match prm {
-                            ReferenceOr::Reference { reference } => insert(&reference)?,
-                            _ => {}
+                            ReferenceOr::Reference { reference } => {
+                                list.push(RefString::Parameter(reference.to_owned()))
+                            }
+                            ReferenceOr::Item(parameter) => match &parameter.schema {
+                                Some(ReferenceOr::Reference { reference }) => {
+                                    list.push(RefString::Schema(reference.to_owned()))
+                                }
+                                Some(ReferenceOr::Item(schema)) => {
+                                    add_refs_for_schema(&mut list, schema)
+                                }
+                                None => {}
+                            },
                         }
                     }
 
                     // responses
                     for (_code, rsp) in &op.responses {
                         match &rsp.schema {
-                            Some(ReferenceOr::Reference { reference }) => insert(&reference)?,
-                            Some(ReferenceOr::Item(_schema)) => {
-                                // TODO properties
-                                // TODO additionalProperties
+                            Some(ReferenceOr::Reference { reference }) => {
+                                list.push(RefString::Schema(reference.to_owned()))
+                            }
+                            Some(ReferenceOr::Item(schema)) => {
+                                add_refs_for_schema(&mut list, schema)
+                            }
+                            None => {}
+                        }
+                    }
+
+                    // examples
+                    for (_name, example) in &op.x_ms_examples {
+                        match example {
+                            ReferenceOr::Reference { reference } => {
+                                list.push(RefString::Example(reference.to_owned()))
                             }
                             _ => {}
                         }
                     }
                 }
             }
+        }
+    }
+
+    // definitions
+    for (_name, schema) in &api.definitions {
+        match schema {
+            ReferenceOr::Reference { reference } => {
+                list.push(RefString::Schema(reference.to_owned()))
+            }
+            ReferenceOr::Item(schema) => add_refs_for_schema(&mut list, schema),
+        }
+    }
+
+    list
+}
+
+/// returns a set of referenced files
+pub fn get_ref_files(api: &OpenAPI) -> Result<IndexSet<String>> {
+    let ref_strings: IndexSet<_> = get_refs(api)
+        .iter()
+        .filter_map(|rf| match rf {
+            RefString::Example(_) => None,
+            rs => Some(rs.to_string()),
+        })
+        .collect();
+
+    let mut set = IndexSet::new();
+    for s in &ref_strings {
+        let rf = Reference::parse(s)?;
+        match rf.file {
+            Some(file) => {
+                set.insert(file);
+                ()
+            }
+            None => {}
         }
     }
 
--- tests/azure_rest_api_specs.rs
+++ tests/azure_rest_api_specs.rs
@@ -5,12 +5,28 @@
 use autorust_codegen::*;
 
 #[test]
+fn refs_count_security_common() -> Result<()> {
+    let api = &read_api_file(
+        "../azure-rest-api-specs/specification/security/resource-manager/common/v1/types.json",
+    )?;
+    let refs = get_refs(api);
+    assert_eq!(13, refs.len());
+    Ok(())
+}
+
+#[test]
+fn refs_count_avs() -> Result<()> {
+    let api = &read_api_file("../azure-rest-api-specs/specification/vmware/resource-manager/Microsoft.AVS/stable/2020-03-20/vmware.json")?;
+    let refs = get_refs(api);
+    assert_eq!(190, refs.len());
+    Ok(())
+}
+
+#[test]
 fn ref_files() -> Result<()> {
     let api = &read_api_file("../azure-rest-api-specs/specification/vmware/resource-manager/Microsoft.AVS/stable/2020-03-20/vmware.json")?;
-    let ref_files: Vec<String> = get_ref_files(api)?.into_iter().collect();
-    assert_eq!(
-        ref_files,
-        vec!["../../../../../common-types/resource-management/v1/types.json".to_owned(),]
-    );
+    let files = get_ref_files(api)?;
+    assert_eq!(1, files.len());
+    assert!(files.contains("../../../../../common-types/resource-management/v1/types.json"));
     Ok(())
 }
