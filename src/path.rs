1395ff4688fade81ae712991a82fa3851099245d
Ryan Levick <ryan.levick@gmail.com>
2020-10-09 10:38:11 +0200

    Small refactorings

--- src/path.rs
+++ src/path.rs
@@ -2,12 +2,16 @@ use crate::Result;
 use path_abs::PathMut;
 use std::path::{Path, PathBuf};
 
-pub fn join<P1: AsRef<Path>, P2: AsRef<Path>>(from_file: bool, a: P1, b: P2) -> Result<PathBuf> {
+/// Joins two files paths together
+///
+/// If the first path ends with a file name (i.e., the last component has a file extension),
+/// the file component is dropped from that path.
+pub fn join<P1: AsRef<Path>, P2: AsRef<Path>>(a: P1, b: P2) -> Result<PathBuf> {
     let mut c = PathBuf::from(a.as_ref());
-    if from_file {
+    if c.extension().is_some() {
         c.pop_up()?; // to directory
     }
-    c.append(b)?;
+    c.append(&b)?;
     Ok(c)
 }
 
@@ -19,7 +23,7 @@ mod tests {
     fn test_path_join() -> Result<()> {
         let a = "../azure-rest-api-specs/specification/vmware/resource-manager/Microsoft.AVS/stable/2020-03-20/vmware.json";
         let b = "../../../../../common-types/resource-management/v1/types.json";
-        let c = join(true, a, b)?;
+        let c = join(a, b)?;
         assert_eq!(
             c,
             PathBuf::from("../azure-rest-api-specs/specification/common-types/resource-management/v1/types.json")
