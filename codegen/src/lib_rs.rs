5a6d5075b38a276bd1e54c6d6bf518e26d9a7545
Cameron Taggart <cameron.taggart@gmail.com>
2020-10-26 20:19:26 -0600

    use TokenCredential in services (#69)

--- codegen/src/lib_rs.rs
+++ codegen/src/lib_rs.rs
@@ -33,13 +33,14 @@ fn create_body(feature_mod_names: &Vec<(String, String)>) -> TokenStream {
             pub api_version: String,
             pub client: reqwest::Client,
             pub base_path: String,
-            pub bearer_access_token: Option<String>,
+            pub token_credential: Option<Box<dyn azure_core::TokenCredential>>,
+            pub token_credential_resource: String,
         }
 
         impl OperationConfig {
-            pub fn new(bearer_access_token: &str) -> Self {
+            pub fn new(token_credential: Box<dyn azure_core::TokenCredential>) -> Self {
                 Self {
-                    bearer_access_token: Some(bearer_access_token.to_owned()),
+                    token_credential: Some(token_credential),
                     ..Default::default()
                 }
             }
@@ -51,7 +52,8 @@ fn create_body(feature_mod_names: &Vec<(String, String)>) -> TokenStream {
                     api_version: API_VERSION.to_owned(),
                     client: reqwest::Client::new(),
                     base_path: "https://management.azure.com".to_owned(),
-                    bearer_access_token: None,
+                    token_credential: None,
+                    token_credential_resource: "https://management.azure.com/".to_owned(),
                 }
             }
         }
